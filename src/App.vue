9376150c5f276087915a574af2aeab55a0c16948
Maydel Rego <maidelrego@yahoo.com>
2022-12-16 12:11:50 -0600

    error handling

--- src/App.vue
+++ src/App.vue
@@ -3,21 +3,30 @@
     <NavBar />
     <v-main>
       <router-view />
+      <SnackbarError />
     </v-main>
   </v-app>
 </template>
 
 <script>
 import NavBar from './NavBar.vue';
+import { mapState } from 'vuex';
+import SnackbarError from '@/misc/SnackbarError.vue';
 import '@/styles/overrides.sass'
 
 export default {
   name: 'App',
   components: {
     NavBar,
+    SnackbarError
   },
   data: () => ({
     //
   }),
+  computed: {
+    ...mapState([
+      'alert',
+    ]),
+  },
 };
 </script>
