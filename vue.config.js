c6b95a2f9db908c16171b28fe13db6a53a4b0b49
Maydel Rego <maidelrego@yahoo.com>
2022-12-17 12:27:45 -0600

    pls work

--- vue.config.js
+++ vue.config.js
@@ -5,15 +5,37 @@ module.exports = defineConfig({
   transpileDependencies: [
     'vuetify'
   ],
-  // output: {
-  //   path: path.resolve(__dirname, '../dist'),
-  //   filename: 'build.js',
-  //   publicPath: './static'
-  // },
+  output: {
+    path: path.resolve(__dirname, '../dist'),
+    filename: 'build.js',
+    publicPath: './static'
+  },
+  configureWebpack: {
+    output: {
+      path: path.resolve(__dirname, '../dist'),
+      filename: 'build.js',
+      publicPath: './static'
+    }
+  },
+  chainWebpack: config => {
+    config
+      .plugin('html')
+      .tap(args => {
+        args[0].title = 'Custom Wood Designs'
+        return args
+      })
+  },
+  css: {
+    loaderOptions: {
+      sass: {
+        additionalData: '@import "@/assets/scss/_variables.scss";'
+      }
+    }
+  },
 })
 
 module.exports = {
-  outputDir: path.resolve(__dirname, '../public'),
+  // outputDir: path.resolve(__dirname,'../public'),
   devServer: {
     proxy: {
       '/api': {
