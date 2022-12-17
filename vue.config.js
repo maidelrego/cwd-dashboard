1af0e5ea02a47e7ce7b040530dad0b1112df3e9a
Maydel Rego <maidelrego@yahoo.com>
2022-12-16 23:22:56 -0600

    deploy 4

--- vue.config.js
+++ vue.config.js
@@ -10,6 +10,7 @@ module.exports = defineConfig({
 module.exports = {
   outputDir: path.resolve(__dirname,'../public'),
   devServer:{
+    allowedHosts: 'all',
     proxy:{
       '/api':{
         target: 'http://localhost:3000'
